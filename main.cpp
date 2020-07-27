5585dac8adc5bcb926f7a0a5eb574dd5df97df05
Michael Carver <carvermichael@gmail.com>
2020-07-27 15:40:04 -0500

    adding initial text system

--- main.cpp
+++ main.cpp
@@ -6,10 +6,14 @@
 #include <sstream>
 #include <vector>
 #include <stdlib.h>
+#include <map>
 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+
+#include <ft2build.h>
+#include FT_FREETYPE_H
 
 #include "camera.h"
 #include "worldState.h"
@@ -386,6 +390,66 @@ void createPlayerVertices() {
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(playerIndices), playerIndices, GL_STATIC_DRAW);
 }
 
+struct TextCharacter {
+	unsigned int textureID;
+	glm::ivec2	 size;
+	glm::ivec2	 bearing;
+	unsigned int advance;
+};
+
+std::map<char, TextCharacter> textCharacters;
+
+unsigned int textVAOID, textVBOID;
+
+// TODO: try doing this without std::string
+void renderText(unsigned int shaderProgramID, std::string text, float x, float y, float scale, glm::vec3 color) {
+
+	glUseProgram(shaderProgramID);
+	unsigned int textColorLoc = glGetUniformLocation(shaderProgramID, "textColor");
+	glUniform3f(textColorLoc, color.x, color.y, color.z);
+
+	glm::mat4 textProjection = glm::ortho(0.0f, (float)SCREEN_WIDTH, 0.0f, (float)SCREEN_HEIGHT);
+	unsigned int projectionLoc = glGetUniformLocation(shaderProgramID, "projection");
+	glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(textProjection));
+
+	glActiveTexture(GL_TEXTURE0);
+	glBindVertexArray(textVAOID);
+
+	std::string::const_iterator c;
+	for (c = text.begin(); c != text.end(); c++) {
+		TextCharacter ch = textCharacters[*c];
+
+		float xPos = x + ch.bearing.x * scale;
+		float yPos = y - (ch.size.y - ch.bearing.y) * scale;
+
+		float w = ch.size.x * scale;
+		float h = ch.size.y * scale;
+
+		float vertices[6][4] = {
+			xPos,		yPos + h, 0.0f, 0.0f,
+			xPos,		yPos,	  0.0f, 1.0f,
+			xPos + w,	yPos,     1.0f, 1.0f,
+
+			xPos,		yPos + h, 0.0f, 0.0f,
+			xPos + w,	yPos,	  1.0f, 1.0f,
+			xPos + w,	yPos + h, 1.0f, 0.0f,
+		};
+
+		glBindTexture(GL_TEXTURE_2D, ch.textureID);
+
+		glBindBuffer(GL_ARRAY_BUFFER, textVBOID);
+		glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices);
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+
+		x += (ch.advance >> 6) * scale; // bit shift changes unit to pixels
+	}
+
+	glBindVertexArray(0);
+	glBindTexture(GL_TEXTURE_2D, 0);
+}
+
 int main() {
 	// ------------ INIT STUFF -------------
 
@@ -470,10 +534,98 @@ int main() {
 
 	Character* characters[2] = { &world.player, &world.theOther };
 	
-	lastFrameTime = (float)glfwGetTime();	
+	lastFrameTime = (float)glfwGetTime();
+
+	// TEXT RENDERING
+	FT_Library ft;
+	if (FT_Init_FreeType(&ft)) {
+		std::cout << "ERROR::FREETYPE: Could not init FreeType Library" << std::endl;
+	}
+
+	FT_Face face;
+	if (FT_New_Face(ft, "arial.ttf", 0, &face)) {
+		std::cout << "ERROR::FREETYPE: Failed to load font" << std::endl;
+	}
+
+	// 0 for width means dynamically adjust based on height
+	FT_Set_Pixel_Sizes(face, 0, 48);
+
+	// generate a texture for each ASCII character
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1); // disable byte-alignment restriction
+
+	for (unsigned char c = 0; c < 128; c++) {
+		if (FT_Load_Char(face, c, FT_LOAD_RENDER)) {
+			std::cout << "ERROR::FREETYTPE: Failed to load Glyph" << std::endl;
+		}
+
+		// generate texture
+		unsigned int texture;
+		glGenTextures(1, &texture);
+		glBindTexture(GL_TEXTURE_2D, texture);
+		glTexImage2D(
+			GL_TEXTURE_2D,
+			0,
+			GL_RED,
+			face->glyph->bitmap.width,
+			face->glyph->bitmap.rows,
+			0,
+			GL_RED,
+			GL_UNSIGNED_BYTE,
+			face->glyph->bitmap.buffer
+		);
+
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+		TextCharacter textCharacter = {
+			texture,
+			glm::ivec2(face->glyph->bitmap.width, face->glyph->bitmap.rows),
+			glm::ivec2(face->glyph->bitmap.width, face->glyph->bitmap_top),
+			face->glyph->advance.x
+		};
+
+		textCharacters.insert(std::pair<char, TextCharacter>(c, textCharacter));
+	}
+
+	FT_Done_Face(face);
+	FT_Done_FreeType(ft);
+
+	//// TEXT SHADER PROGRAM
+	unsigned int textVertexShaderID = initializeVertexShader("textVertexShader.vert");
+	unsigned int textFragShaderID   = initializeFragmentShader("textFragmentShader.frag");
+	unsigned int textShaderProgramID = createShaderProgram(textVertexShaderID, textFragShaderID);
+
+	// need alpha blending for text transparency
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+	// reserving data for text on gpu
+	glGenVertexArrays(1, &textVAOID);
+	glGenBuffers(1, &textVBOID);
+	glBindVertexArray(textVAOID);
+	glBindBuffer(GL_ARRAY_BUFFER, textVBOID);
+
+	/*
+		Note the NULL:
+		So far, in other calls like this, we give a pointer to the data at the same time we describe the data.
+		However, this time we're going to create each text character's vertices on the fly (as they each have their
+		own spacial needs. So, this first Data call just describes the data, and the subData call later sends along
+		the vertices. -- carver (7-27-20)
+	*/
+	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 6 * 4, NULL, GL_DYNAMIC_DRAW);
+	
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(float), 0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
 
 	// game loop
+	
 	while (!glfwWindowShouldClose(window)) {
+		glUseProgram(shaderProgramID);
+		
 		processKeyboardInput(window);
 		
 		glfwPollEvents();
@@ -486,7 +638,9 @@ int main() {
 		glClearColor(0.0f, 0.7f, 0.3f, 1.0f);
 
 		// DRAW CURRENT GRID (where the player currently is)
+		glUseProgram(shaderProgramID);
 		glBindVertexArray(cube_VAO_ID);
+		
 		for (int row = 0; row < GRID_MAP_SIZE_X; row++) {
 			float yOffset = -0.5f * row;
 			
@@ -532,6 +686,8 @@ int main() {
 			glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(current_model));
 			glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
 		}
+
+		renderText(textShaderProgramID, "AWWWWWW YEAH", 10.0f, 10.0f, 1.0f, glm::vec3(1.0f, 0.5f, 0.89f));
 
 		float currentFrame = (float)glfwGetTime();
 		deltaTime = currentFrame - lastFrameTime;
