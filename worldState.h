d7ea7a4b2016f4aa7256f844193d931bafb7eaf2
Michael Carver <carvermichael@gmail.com>
2020-12-19 11:38:21 -0600

    switching away from multithread stuff, starting to clean a bit

--- worldState.h
+++ worldState.h
@@ -54,27 +54,6 @@ struct WorldState {
     }
 };
 
-my_vec3 gridCoordsToWorldOffset(my_ivec3 gridCoords) {
-    my_vec3 worldOffset;
-
-    worldOffset.x = (float) gridCoords.x;
-    worldOffset.y = (float)-gridCoords.y;
-    worldOffset.z = (float) gridCoords.z;
-
-    return worldOffset;
-}
-
-my_ivec3 worldOffsetToGridCoords(my_vec3 worldOffset) {
-    
-    my_ivec3 gridCoords;
-
-    gridCoords.x = (int)( worldOffset.x); 
-    gridCoords.y = (int)(-worldOffset.y); 
-    gridCoords.z = (int)( worldOffset.z); 
-    
-    return gridCoords;
-}
-
 #define MAX_ENTITIES 100
 #define NUM_TURN_ENTITIES 2
 
